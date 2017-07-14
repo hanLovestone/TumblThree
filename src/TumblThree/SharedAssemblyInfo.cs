67f75bcf4ebe4257b94df9f821b6f57415338d2d
Johannes Meyer zum Alten Borgloh <code@jzab.de>
2017-07-14 22:02:34 +0200

    Tumblr raw image improvements
    - If Tumblr raw images are specified as size for downloading, the hosts
    in the tumblrHosts array are tested for _raw images. If all generated
    URIs return an error code, the detected host from the crawler is tried
    as a last option.

--- src/TumblThree/SharedAssemblyInfo.cs
+++ src/TumblThree/SharedAssemblyInfo.cs
@@ -12,5 +12,5 @@ using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
 [assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.MainAssembly)]
-[assembly: AssemblyVersion("1.0.5.24")]
-[assembly: AssemblyFileVersion("1.0.5.24")]
+[assembly: AssemblyVersion("1.0.5.25")]
+[assembly: AssemblyFileVersion("1.0.5.25")]
