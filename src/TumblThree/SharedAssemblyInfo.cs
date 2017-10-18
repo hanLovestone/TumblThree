9c5211566b5ab3b0c17b74a8847c82f9581308d5
Johannes Meyer zum Alten Borgloh <code@jzab.de>
2017-10-18 07:31:48 +0200

    Bugfixes
    - Allows to leave one date unset if posts should only be downloaded from
    or to a specific date.
    - Fixes incomplete photo and video meta data download which was
    introduced in the commit 0435631.

--- src/TumblThree/SharedAssemblyInfo.cs
+++ src/TumblThree/SharedAssemblyInfo.cs
@@ -12,5 +12,5 @@ using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
 [assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.MainAssembly)]
-[assembly: AssemblyVersion("1.0.5.52")]
-[assembly: AssemblyFileVersion("1.0.5.52")]
+[assembly: AssemblyVersion("1.0.5.53")]
+[assembly: AssemblyFileVersion("1.0.5.53")]
